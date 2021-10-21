961fdf1d6369b702bc1cf6a6a9306a0878e57d65
AndrewHatch <03110h@gmail.com>
2021-10-21 11:08:19 -0400

    bugfix

--- routes/albums.js
+++ routes/albums.js
@@ -2,17 +2,41 @@ const express = require('express');
 const router = express.Router();
 //import data from data.json
 const dataFile = require('../data/data.json')
-
-//{band:[{},{},{}]}
-
+let pageAlbums = dataFile.albums
 
 router.get('/albums', (req,res) => {
-
-
+    //display all albums
+    let albumCovers =[]
+    pageAlbums.forEach(albumObj =>{
+        albumCovers = albumCovers.concat(albumObj.artwork)
+    })
     res.render('albums',{
+        artwork: albumCovers,
+        albums: pageAlbums,
+        pageTitle: "Albums | Unleash the Archers"
+
         
     })
 
 })
+router.get('/albums/:albumid',(req,res) => {
+    //display one album
+    let album = req.params.speakerid
+    let photos = []
+    let albums = []
 
+    pageAlbums.forEach(albumObj =>{
+        if( albumObj.shortname === album){
+            photos = [...albumObj.artwork]
+            albums.push(albumObj)
+        }
+    })
+    
+    res.render('albums',{
+        artwork: albumCovers,
+        albums: pageAlbums,
+        pageTitle: `${albums[0].name} | Unleash the Archers`
+
+    })
+})
 module.exports = router
